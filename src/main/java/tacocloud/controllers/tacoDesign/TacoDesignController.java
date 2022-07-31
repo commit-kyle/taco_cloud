266ff8d547ee44acbe36ca81785753863734b987
Kyle Buttress <kyle_buttress@kyle.buttress>
2022-08-01 00:11:48 +0200

    feat (Controller): Adding method to handle @Post request for form submission. Persistence logic to follow.

--- src/main/java/tacocloud/controllers/tacoDesign/TacoDesignController.java
+++ src/main/java/tacocloud/controllers/tacoDesign/TacoDesignController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 
 import tacocloud.domains.ingredient.Ingredient;
 import tacocloud.domains.ingredient.Ingredient.Type;
+import tacocloud.domains.taco.Taco;
 
 
 @Slf4j
@@ -41,7 +43,15 @@ public class TacoDesignController {
 
         model.addAttribute("design", new Taco());
 
-        return "design";
+        return "design/design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        // Persistence logic to follow
+        log.info("Processing Design: " + design);
+
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
