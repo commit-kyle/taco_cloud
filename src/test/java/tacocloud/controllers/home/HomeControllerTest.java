59f8c5464033fc5f00c0d82ffba73784aa210319
Kyle Buttress <kyle_buttress@kyle.buttress>
2022-07-31 02:01:10 +0200

    Test - Home controller

---
+++ src/test/java/tacocloud/controllers/home/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package tacocloud.controllers.home;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        CoreMatchers.containsString("Welcome to Taco Cloud"))
+                );
+    }
+}
