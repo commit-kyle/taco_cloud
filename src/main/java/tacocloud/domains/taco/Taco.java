1b6486d1bfd32994d71d0d72ec586fcd73a10c84
Kyle Buttress <kyle_buttress@kyle.buttress>
2022-08-01 21:51:18 +0200

    fix: Added validation annotations

--- src/main/java/tacocloud/domains/taco/Taco.java
+++ src/main/java/tacocloud/domains/taco/Taco.java
@@ -2,10 +2,16 @@ package tacocloud.domains.taco;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min = 5, message = "Name should be at least 5 characters long")
     private String name;
+
+    @Size(min = 1, message = "Please select at least one ingredient")
     private List<String> ingredients;
 }
